24801f24fcede9c5c9209f19d802cf2ee28bda6e
Michael Young <michaeljy@gmail.com>
2015-12-20 01:43:50 -0600

    Lecture 21 finished

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -1,53 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
-var React = require('react-native');
-var {
-  AppRegistry,
-  StyleSheet,
+import React from 'react-native';
+let {
   Text,
   View,
-} = React;
+  AppRegistry
+  } = React;
 
-var stopwatch = React.createClass({
-  render: function() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+let StopWatch = React.createClass({
+  render() {
+    return <Text>
+      Hi.
+    </Text>
   }
 });
 
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+// Show the react component on the screen
+AppRegistry.registerComponent('stopwatch', () => {
+  return StopWatch;
 });
-
-AppRegistry.registerComponent('stopwatch', () => stopwatch);
