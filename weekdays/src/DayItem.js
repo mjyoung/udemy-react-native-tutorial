f4e7a97970eb9eb3c658d5c19009c6f398126832
Michael Young <michaeljy@gmail.com>
2015-12-20 01:16:42 -0600

    Lecture 19 finished.

--- weekdays/src/DayItem.js
+++ weekdays/src/DayItem.js
@@ -1,23 +1,42 @@
 // Import some code we need
 import React from 'react-native';
 let Text = React.Text;
-let StyleSheet = React.StyleSheet;
 
 // Create our component
 let DayItem = React.createClass({
   render() {
-    return <Text style={styles.day}>
+    return <Text style={this.style()}>
       {this.props.day}
     </Text>
-  }
-});
+  },
 
-// Style our component
-var styles = StyleSheet.create({
-  day: {
-    fontSize: 18,
-    color: '#0000ff'
+  style() {
+    return {
+      color: this.color(),
+      fontWeight: this.fontWeight(),
+      fontSize: this.fontSize(),
+      lineHeight: this.lineHeight()
+    }
+  },
+
+  color() {
+    var opacity = 1 / (this.props.daysUntil + 1);
+    return 'rgba(0, 0, 0, ' + opacity + ')';
+  },
+
+  fontWeight() {
+    var weight = (8 - this.props.daysUntil) * 100;
+    return weight.toString();
+  },
+
+  fontSize() {
+    return 60 - 6 * this.props.daysUntil;
+  },
+
+  lineHeight() {
+    return 70 - 4 * this.props.daysUntil;
   }
+
 });
 
 // Make this code available elsewhere
