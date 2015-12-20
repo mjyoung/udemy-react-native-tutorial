c2450091a72a45d2143832372dc3a65fe911d559
Michael Young <michaeljy@gmail.com>
2015-12-20 00:26:52 -0600

    Lecture 10 finished.

--- weekdays/index.ios.js
+++ weekdays/index.ios.js
@@ -3,15 +3,25 @@ import React from 'react-native';
 let AppRegistry = React.AppRegistry;
 let View = React.View;
 let Text = React.Text;
+let StyleSheet = React.StyleSheet;
 
 // Create a react component
 let Weekdays = React.createClass({
   render() {
-    return <View>
+    return <View style={styles.container}>
       <Text>
         Days of the week:
       </Text>
     </View>
+  }
+});
+
+// Style the React component
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
