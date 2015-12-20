ce25546f7749e5a89eb9472b7d7eaf9c4fc4638e
Michael Young <michaeljy@gmail.com>
2015-12-20 02:19:43 -0600

    Lecture 25 finished.

--- stopwatch/index.ios.js
+++ stopwatch/index.ios.js
@@ -10,12 +10,12 @@ let StopWatch = React.createClass({
   render() {
     return <View style={styles.container}>
       <View style={[styles.header, this.border('yellow')]}>
-        <View style={this.border('red')}>
+        <View style={[styles.timerWrapper, this.border('red')]}>
           <Text>
             00:00.00
           </Text>
         </View>
-        <View style={this.border('green')}>
+        <View style={[styles.buttonWrapper, this.border('green')]}>
           {this.startStopButton()}
           {this.lapButton()}
         </View>
@@ -54,13 +54,24 @@ let StopWatch = React.createClass({
 let styles = StyleSheet.create({
   container: {
     flex: 1, // Fill the entire screen
-    alignItems: 'stretch',
+    alignItems: 'stretch'
   },
   header: { // Yellow
-    flex: 1,
+    flex: 1
   },
   footer: { // Blue
-    flex: 1,
+    flex: 1
+  },
+  timerWrapper: { // Red
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonWrapper: { // Green
+    flex: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   }
 });
 
