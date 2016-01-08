fcf5a59b18342d68f0ed6d8e2ac0a38201caacac
Michael Young <michaeljy@gmail.com>
2016-01-08 02:32:19 -0600

    Lecture 58 finished. Inputs and State.

--- authentication/src/components/authentication/signin.js
+++ authentication/src/components/authentication/signin.js
@@ -6,17 +6,53 @@ let {
   TextInput
 } = React;
 
+import Button from '../common/button.js';
+
 module.exports = React.createClass({
+
+  getInitialState() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Sign In</Text>
         <Text style={styles.label}>Username:</Text>
-        <TextInput style={styles.input} />
+        <TextInput
+          style={styles.input}
+          value={this.state.username}
+          // onChangeText={(text) => this.setState({username: text})}
+          onChangeText={this.setUsername}
+          />
         <Text style={styles.label}>Password:</Text>
-        <TextInput secureTextEntry={true} style={styles.input} />
+        <TextInput
+          secureTextEntry={true}
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={this.setPassword}
+          />
+        <Button
+          text={'Sign In'}
+          onPress={this.onPress} />
+        <Text>{this.state.username} - {this.state.password}</Text>
       </View>
     );
+  },
+
+  setUsername(inputValue) {
+    this.setState({ username: inputValue });
+  },
+
+  setPassword(inputValue) {
+    this.setState({ password: inputValue });
+  },
+
+  onPress() {
+    // log the user in
   }
 });
 
